0994661c8796c1861496916e00c47e5e0d0c35d8
KryptoKazz <kryptokazz@pop-os.localdomain>
2021-11-18 09:20:28 +1000

    modified:   Klaos.sol

--- packages/hardhat/contracts/Klaos.sol
+++ packages/hardhat/contracts/Klaos.sol
@@ -4,7 +4,8 @@ pragma solidity >=0.8.0 <0.9.0;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Klaos is ERC20 {
+contract  is ERC20 {
+
   address public admin;
     constructor() ERC20('Klaos' , 'PHI') {
       _mint(msg.sender, 10000 * 10 ** 18);
@@ -15,29 +16,6 @@ contract Klaos is ERC20 {
       require(msg.sender == admin, 'only admin');
       _mint(to, amount);
     }
-
-    function burn(uint amount) external {
-      _burn(msg.sender,amount);
-
-    }
 }
 
 
-
-
-//Foot Note
-/*
-
-
-Matthew 14:19
-"And he directed the people to sit down on the grass.
-Taking the five loaves and the two fish and looking up to heaven,
-he gave thanks and broke the loaves.
-Then he gave them to the disciples, and the disciples gave them to the people."
-
-In the Greek translation the breaking of the bread uses the verb "klao"
-meaning to break apart. This meaning was the inspiration for the name of this project
-as it is about breaking plots of land into micro deeds.
-
-
-*/
