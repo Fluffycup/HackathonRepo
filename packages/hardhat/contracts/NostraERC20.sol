e61dee5d0999a05a5ec913a473b83178ace568e5
KryptoKazz <kryptokazz@pop-os.localdomain>
2021-11-18 11:30:42 +1000

    deleted:    KlaoNFT.sol 	deleted:    Klaos.sol 	deleted:    KlaosNFT.sol 	modified:   NostraERC20.sol

--- packages/hardhat/contracts/NostraERC20.sol
+++ packages/hardhat/contracts/NostraERC20.sol
@@ -3,8 +3,9 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract  is ERC20, ownable {
+contract NostraDomus is ERC20, Ownable {
   address public admin;
     constructor() ERC20('NostraDomus' , 'Nostra') {
       _mint(msg.sender, 10000 * 10 ** 18);
